0af7367c362d18ff0c4f22c31e3ea1b93e7f40b8
talSchreiber95 <talfreestyle@gmail.com>
2020-12-02 23:37:21 +0200

    add openaAcoount

--- myBank.c
+++ myBank.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include "myBank.h"
 #define N 50
-
-int accounts[N][2];
-
-void openAccount(int accoutNumber, double initialDeposit) {
-
+#define DEL 901
+// N=50 , 2 means first for account deposit value number secound for 0/1 (if the account exist or not--> 0=doesn't exist , 1= exist)
+double accounts[N][2];
+static int times=0;
+void openAccount(double initialDeposit) {
         printf("OpenAccount()");
+        if(times<N){
+            int acc=-1;
+            for (int i = 0; i < N; i++)
+            {
+                  if(accounts[i][1]==0)
+                  {
+                    acc=i;
+                    break;
+                  }
+            }
+            if(acc!=-1){
+                accounts[acc][0]=initialDeposit;
+                printf("account created");
+            }
+        }
+        else {
+            printf("max accounts");
+        }
 }
 
 double balance(int accountNumber) {
-
-
+    int acc = DEL-accountNumber;
+    
     return 0.0;
 }
 
