7e6c4e0a4dc6aa1e57c5a16a267114a3172cf1c1
Itay Sharabi <itay.nextweb@gmailcom>
2020-12-06 22:53:39 +0200

    Finally finished Ex2

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 #define N 50
 
+int main()
+{
 
-int main() {
-    
     int isRunning = 1;
     double sum, interest;
     int accountNumber;
@@ -17,61 +17,108 @@ int main() {
         printf("Please choose transaction type: \n O - Open account\n B - Balance\n D - Deposit\n W - Withdraw\n C - Close account\n I - Interest\n P - Print\n E - Exit\n");
         scanf(" %c", &input);
 
-        switch (input) 
+        switch (input)
         {
 
         default:
-        printf("Invalid transaction type!\n");
+            printf("Invalid transaction type!\n");
             break;
         case 'O':
             printf("Please enter amount for deposit:\n ");
             // scanf(" %lf", &sum);
             if (scanf(" %lf", &sum) == 1)
             {
-                if (sum > 0) 
+                if (sum > 0)
+                {
+                    openAccount(sum);
+                }
+                else
+                {
+                    printf("Invalid amount\n");
+                }
+            }
+            else
             {
-                openAccount(sum);
-            } else {
-                printf("Invalid amount\n");
+                printf("Invalid sum");
             }
-            }
-            
-            
+
             break;
 
         case 'B':
             printf("Please enter an account number: ");
-            scanf(" %d", &accountNumber);
-            // printf("Balance of account %d is: %lf \n", accountNumber, balance(accountNumber));
+            if (scanf(" %d", &accountNumber) == 1)
+            {
+                printf("Balance of account %d is: %lf \n", accountNumber, balance(accountNumber));
+            }
+            else
+            {
+                printf("Invalid account number");
+            }
             break;
 
         case 'D':
             printf("Please enter an account number: ");
-            scanf(" %d", &accountNumber);
-            printf("\nEnter amount to deposit: ");
-            scanf(" %lf", &sum);
-            // printf("New balance of account %d is: %lf \n", accountNumber, deposit(accountNumber, sum));
+            if (scanf(" %d", &accountNumber) == 1)
+            {
+                printf("\nEnter amount to deposit: ");
+                if (scanf(" %lf", &sum) == 1.0)
+                {
+                    printf("New balance of account %d is: %lf \n", accountNumber, deposit(accountNumber, sum));
+                }
+                else
+                {
+                    printf("Invalid sum to deposit");
+                }
+            }
+            else
+            {
+                printf("Invalid account number");
+            }
             break;
 
         case 'W':
             printf("Please enter an account number: ");
-            scanf(" %d", &accountNumber);
-            printf("\nEnter amount to withdraw: ");
-            scanf(" %lf", &sum);
-            withdraw(accountNumber, sum);
+            if (scanf(" %d", &accountNumber) == 1)
+            {
+                printf("\nEnter amount to withdraw: ");
+                if (scanf(" %lf", &sum) == 1)
+                {
+                    withdraw(accountNumber, sum);
+                }
+                else
+                {
+                    printf("Invalid sum to withdraw");
+                }
+            }
+            else
+            {
+                printf("Invalid account number");
+            }
             break;
-        
+
         case 'C':
             printf("Please enter your account number: ");
-            scanf(" %d", &accountNumber);
-            closeAccount(accountNumber);
-            printf("\n");
+            if (scanf(" %d", &accountNumber) == 1)
+            {
+                closeAccount(accountNumber);
+                printf("\n");
+            }
+            else
+            {
+                printf("Invalid account number");
+            }
             break;
 
         case 'I':
             printf("Enter interest amount: ");
-            scanf(" %lf", &interest);
-            incInterest(interest);
+            if (scanf(" %lf", &interest) == 1)
+            {
+                incInterest(interest);
+            }
+            else
+            {
+                printf("Invalid interest amount");
+            }
             break;
 
         case 'P':
